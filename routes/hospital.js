8ef618ab3692771b766ff9a1fc543383b60c082c
Proud <Proud_passamon@hotmail.co.th>
2023-02-20 00:07:23 +0700

    Assignment6

---
+++ routes/hospital.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const {protect, authorize} = require('../middleware/auth');
+
+const {getHospital, getHospitals, createHospital, updateHospitals, deleteHospitals} = require('../controllers/hospital');
+
+router.route('/').get(getHospitals).post(protect, authorize('admin'), createHospital);
+router.route('/:id').get(getHospital).put(protect, authorize('admin'), updateHospitals).delete(protect, authorize('admin'), deleteHospitals);
+
+module.exports=router;
